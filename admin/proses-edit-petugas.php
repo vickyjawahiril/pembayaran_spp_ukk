68c6eaca168259b48b2b2c35ff88c16f5693e33e
vickyjawahiril <vickyjawahiril2@gmail.com>
2023-02-14 13:34:54 +0700

    pembayaran

--- admin/proses-edit-petugas.php
+++ admin/proses-edit-petugas.php
@@ -1,12 +1,13 @@
 <?php
 
-$passowrd = $_GET['username'];
+$id_petugas = $_GET['id_petugas'];
+$username = $_POST['username'];
 $password = $_POST['password'];
 $nama_petugas = $_POST['nama_petugas'];
 $level = $_POST['level'];
 
 include '../koneksi.php';
-$sql = "UPDATE petugas SET username='$username', password='$password'nama_petugas='$nama_petugas',level='$level', WHERE id_petugas='$id_petugas'";
+$sql = "UPDATE petugas SET username='$username', password='$password', nama_petugas='$nama_petugas', level='$level' WHERE id_petugas='$id_petugas'";
 $query = mysqli_query($koneksi, $sql);
 if($query){
     header("location:?url=petugas");
